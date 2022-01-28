cc91f3bd1203ceb94bffd625543e114b6bd58201
yiti <729390400@qq.com>
2022-01-28 09:49:19 +0800

    version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-All notable changes to the "side" extension will be documented in this file.
+All notable changes to the "tt" extension will be documented in this file.
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
 import * as nls from 'vscode-nls';
 const localize=nls.config({messageFormat: nls.MessageFormat.both})();
 
@@ -11,15 +12,17 @@ export function activate(context: vscode.ExtensionContext) {
 	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "tt" is now active!');
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('side.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('tt.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
     const message = localize('sayHello.text', 'Hello World');
     vscode.window.showInformationMessage(message);
-		// vscode.window.showInformationMessage('Hello World from side!');
+		vscode.window.showInformationMessage('Hello World from tt!');
 	});
 
 	context.subscriptions.push(disposable);
